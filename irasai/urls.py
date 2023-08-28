14e97f09cc29fed584060dfe5539a4bea2371252
Regis <r.petrauskui@gmail.com>
2023-08-28 11:23:15 +0300

    vienas artistas viewsas(testuojam rankiniu budu,  adrese prirasom id)

--- irasai/urls.py
+++ irasai/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='homepage-url'),
     path('artistai/', views.artistai, name='artistai-visi-url'),
+    path('artistai/<int:artistas_id>', views.artistas, name='artistas-vienas-url'),
 
 ]
--- irasai/views.py
+++ irasai/views.py
@@ -25,3 +25,11 @@ def artistai(request):
     }
     return render(request, 'artistai_visi.html', context=context_t)
 
+
+def artistas(request, artistas_id):
+    artistas_viena_eilute = get_object_or_404(Artistas, pk=artistas_id)
+    context_t = {
+        'artistas_viena_eilute_t': artistas_viena_eilute
+    }
+    return render(request, 'artistas_vienas.html', context=context_t)
+
